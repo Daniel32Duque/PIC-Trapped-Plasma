831202165f90d10400b5ed55de66996d5e70c08d
Daniel32Duque <59413316+Daniel32Duque@users.noreply.github.com>
2020-03-04 16:22:55 +0000

    Boltzmann Constant

--- Source/Constants.hpp
+++ Source/Constants.hpp
@@ -1,6 +1,6 @@
 /*
 Written by: Daniel Duque
-Last modified on 19 Nov 2019
+Last modified on 04 Mar 2020
 
 Declarations for the Plasma class
 This file contains a corresponding source file.
@@ -13,5 +13,6 @@ const double ePos{ 1.602176634e-19 }; //Elementary positive charge in Coulombs
 const double epsilon{ 8.8541878128e-12 }; //Permittivity of free space in SI units
 const double massE{ 9.1093837015e-31 }; //Electron rest mass in kilograms
 const double PI{ 3.14159265359 };
+const double KB{ 1.38064852e-23 }; //Boltzmann constant in SI
 
 #endif
