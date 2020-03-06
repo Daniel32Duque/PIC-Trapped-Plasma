33e00865f13171d424d025ffc56a1176e10c1c14
Daniel32Duque <59413316+Daniel32Duque@users.noreply.github.com>
2020-03-06 20:03:19 +0000

    Update Plasma.hpp

--- Source/Plasma.hpp
+++ Source/Plasma.hpp
@@ -1,6 +1,6 @@
 /*
 Written by: Daniel Duque
-Last modified on 04 Mar 2020
+Last modified on 06 Mar 2020
 
 Declarations for the Plasma class
 This file contains a corresponding source file.
@@ -14,18 +14,17 @@ This file contains a corresponding source file.
 #include<Eigen/SparseLU>
 #include<array>
 #include<vector>
+#include<iostream>
+#include<random>
 
 class MacroRing
 {
 private:
-	int posR;//Fixed at one of the grid divisions along r
-	double posZ; //Position along z
+	int posR;//Fixed at one of the grid divisions along r. This indicates the index of the radial grid point [0,Nr]
+	double posZ; //Position along z. Position in meters along the trap [0, trapLength]
 	double speed; //speed along z, there is no r transfer.
 	std::vector<double> historySpeed;
 	std::vector<double> historyZ;
-public:
-	MacroRing(int r, double z, double aSpeed);
-	~MacroRing();
 	int getR() const;
 	double getZ() const;
 	double getSpeed() const;
@@ -35,6 +34,8 @@ public:
 	void reserve(int desired); //Saves space for how many times you will save state.
 	void printPositions(std::ofstream& file) const;
 	void printSpeeds(std::ofstream& file) const;
+	MacroRing(int r, double z, double aSpeed);
+	friend Plasma;
 };
 
 class Plasma
@@ -42,14 +43,12 @@ class Plasma
 private:
 	PenningTrap& refTrap;
 	const std::string name;//A name to identify the output files e.g. Electrons, Antiprotons, etc.
-	const double mass; //Mass of type of particles that form the plasma. It is NOT the total mass of the plasma.
-	const double charge; //Not the net charge of the plasma. It is the charge of the particle that forms the plasma.
-	double chargeMacro; //Charge of each MacroRing, all rings have the same charge regardless of r position.
-	double massMacro; //Mass of each MacroRing.
-	std::vector<MacroRing> rings;
-	double temperature;
-	double totalCharge;
-	double centralDensity;
+	const double mass; //Mass of type of particles that form the plasma. It is NOT the total mass of the plasma. (In kg)
+	const double charge; //Not the net charge of the plasma. It is the charge of the particle that forms the plasma. (In Coulombs)
+	double chargeMacro; //Charge of macro ring at r=0. The charge of a general macro ring at a given r is given by (indexR == 0 ? macroCharge : 8 * indexR * macroCharge)
+	double macroChargeDensity; //Charge increases in the same proportion as volume along r. Each macroparticle has a fixed charge density in Coulombs per meter cubed
+	std::vector<MacroRing> rings; //All macro-Rings of a given plasma are stored here
+	double temperature; //In Kelvin
 	Eigen::VectorXd selfPotential{ refTrap.Nz * refTrap.Nr + refTrap.Nr };
 	Eigen::VectorXd RHS{ refTrap.Nz * refTrap.Nr + refTrap.Nr };// = - charge density / permittivity of free space
 	void updateRHS();
@@ -62,14 +61,14 @@ private:
 	Eigen::VectorXd initialDensity{ refTrap.Nz * refTrap.Nr + refTrap.Nr };// Initial density with which the plasma was loaded
 	void estimateDensityProportions(); //Estimate the densities w.r.t. central densities in local thermal equilibrium from total potential. i.e. a value of 1 for all radius at the centre. Need to determine the actual values based on the profile
 	void fitDensityProportionToProfile(double shape, double scale); //Weight each radius to match the desired profile
-	void normalizeDensityToTotalCharge(); //Weight each radius so that the total charge matches the desired total charge
+	void normalizeDensityToTotalCharge(double totalCharge); //Weight each radius so that the total charge matches the desired total charge
 public:
 	Plasma(PenningTrap& trap, std::string name, double mass, double charge);
 	~Plasma();
 	friend PenningTrap;
 	void extractSelfPotential(std::string fileName) const;//Store the potential field in a document called filename
 	void extractPlasmaParameters(std::string filename) const;//Extract the plasma parameters
-	void extractInitialDensity(std::string filename) const;
+	void extractInitialDensity(std::string filename) const;//This is NOT the actual initial density obtained from macro-Particles. This is the EXPECTED initial density
 	//Loading routines
 	/*----------------------------------------------------------------------------------
 	These routines below are the only thing that you should change/add to the code.
@@ -77,7 +76,7 @@ public:
 	into the trap.
 	The template for a loading routine should be:
 	1) Check that input makes sense.
-	2) Assign charge and mass of a MacroRing (same for all).
+	2) Assign charge and charge density of a MacroRing (same for all).
 	3) Clear the rings vector (This is like cleaning the trap before loading a plasma).
 	4) Push the MacroRings into the rings vector according to the loading you want.
 	5) Solve Poisson's Equation. Just call the method; literally one line: solvePoisson();
@@ -85,8 +84,8 @@ public:
 	The code only requires the loading to populate the rings vector i.e. telling the code how
 	and where are the particles initially.
 	-------------------------------------------------------------------------------------*/
-	void loadOneDUniform(int numMacro, double chargeMacro, double lengthLine, int r); //equally spaced particles at fixed r in a length centred in the center of the trap.
-	void loadSingleRing(double chargeMacro, int r, double Z, double speed); //Single ring at a position with a velocity
+	void loadOneDUniform(int numMacro, double totalCharge, double lengthLine, int r); //equally spaced particles at fixed r in a length centred in the center of the trap.
+	void loadSingleRing(double totalCharge, int r, double Z, double speed); //Single ring at a position with a velocity
 	void loadProfile(double aTemperature, double totalCharge, double shape, double scale, int numMacro, double KSThreshold); //Load using a density profile (as obtained from MCP), shape and scale define the generalized normal fit. Assume local thermal equilibrium along each radius
 };
 
