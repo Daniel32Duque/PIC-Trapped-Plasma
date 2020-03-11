c6c4a87cf48b54f31cb8780d2583d28a4474f150
Daniel32Duque <59413316+Daniel32Duque@users.noreply.github.com>
2020-03-11 17:07:40 +0000

    Load from Density File

--- Source/Plasma.cpp
+++ Source/Plasma.cpp
@@ -1,9 +1,9 @@
 /*
 Written by: Daniel Duque
-Last modified on 07 Mar 2020
+Last modified on 11 Mar 2020
 
 Definitions for the Plasma class
-This file contains a corresponding source file.
+This file contains a corresponding header file.
 */
 #include"Plasma.hpp"
 
@@ -144,6 +144,10 @@ void Plasma::extractInitialDensity(std::string fileName) const
 	newFile << initialDensity.format(fastFullPrecision);
 	newFile.close();
 }
+int Plasma::getNumMacro() const
+{
+	return rings.size();
+}
 void Plasma::extractHistory(std::string preName) const
 {
 	std::ofstream newPositions, newSpeeds;
@@ -261,6 +265,10 @@ void Plasma::loadOneDUniform(int numMacro, double aTotalCharge, double lengthLin
 	{
 		throw std::logic_error("Charge of MacroRing and the plasma type must have the same sign");
 	}
+	if (numMacro <= 0)
+	{
+		throw std::logic_error("Number of macro-particles has to be a positive integer");
+	}
 	aTotalCharge /= numMacro; //Charge of a single macro-ring
 	chargeMacro = r == 0 ? aTotalCharge : aTotalCharge / (8 * r);
 	macroChargeDensity = 4 * chargeMacro / (PI * refTrap.hz * refTrap.hr * refTrap.hr);
@@ -298,6 +306,10 @@ void Plasma::loadProfile(double aTemperature, double aTotalCharge, double shape,
 	if (aTemperature <= 0 || shape <= 0 || scale <= 0)
 	{
 		throw std::logic_error("Temperature, shape, and scale all need to be positive");
+	}
+	if (numMacro <= 0)
+	{
+		throw std::logic_error("Number of macro-particles has to be a positive integer");
 	}
 	if (aTotalCharge * charge < 0)
 	{
@@ -384,6 +396,99 @@ void Plasma::loadProfile(double aTemperature, double aTotalCharge, double shape,
 		//Now combine both the new and old density based on the KSCorrection
 		//This linear combinations retains the total integral and the profile
 		initialDensity = KSCorrection * previousDensity + (1 - KSCorrection) * initialDensity;
+	}
+	//Ok, now I have a density grid. I need now to populate macro-particles to match this grid density.
+	//How many particles am I placing at each radial index
+	std::vector<std::vector<double>> cumulativeAtR;
+	for (int indexR = 0; indexR < refTrap.Nr; ++indexR)
+	{
+		std::vector<double> cumulative;
+		double volume;
+		if (indexR == 0)
+		{
+			volume = PI * refTrap.hz *  refTrap.hr *  refTrap.hr / 4;
+		}
+		else
+		{
+			volume = refTrap.hz *  refTrap.hr * 2 * PI * indexR *  refTrap.hr;
+		}
+		double aChargeR{ 0 };
+		for (int indexZ = 0; indexZ < refTrap.Nz + 1; ++indexZ)
+		{
+			aChargeR += volume * initialDensity.coeffRef((refTrap.Nz + 1) * indexR + indexZ);
+			cumulative.push_back(aChargeR);
+		}
+		cumulativeAtR.push_back(cumulative);
+	}
+	double futureMacroCharge{ cumulativeAtR[0].back() };
+	for (unsigned int i = 1; i < cumulativeAtR.size(); ++i)
+	{
+		futureMacroCharge += cumulativeAtR[i].back() / (8 * i);
+	}
+	chargeMacro = futureMacroCharge / numMacro;
+	macroChargeDensity = 4 * chargeMacro / (PI * refTrap.hz * refTrap.hr * refTrap.hr);
+	std::vector<int> numAtR;
+	numAtR.push_back((int)round(cumulativeAtR[0].back() / chargeMacro));
+	for (unsigned int i = 1; i < cumulativeAtR.size(); ++i)
+	{
+		numAtR.push_back((int)round(cumulativeAtR[i].back() / (8 * i * chargeMacro)));
+	}
+	//Now I know how many particles I want to place at each radius.
+	//Distribute them using an inverse transform sampling
+	//But without any randomness, just equally space the uniform distribution
+	rings.clear();
+	rings.reserve(numMacro);//this is not exactly the number of particles we will load, but it is close
+	//Use std::random to produce boltzmann distributed speeds
+	//this are simply gaussian with the appropriate std deviation as we are looking at 1 dimension only
+	std::default_random_engine generator;
+	std::normal_distribution<double> distribution(0, sqrt(KB * aTemperature / mass));
+	for (int indexR = 0; indexR < refTrap.Nr; ++indexR)
+	{
+		double deltaQ{ cumulativeAtR[indexR].back() / (numAtR[indexR] + 1) };
+		int currentIndex{ 0 };
+		for (int i = 0; i < numAtR[indexR]; ++i)
+		{
+			double currentInvert{ deltaQ * (i + 1) };
+			while (abs(cumulativeAtR[indexR][currentIndex]) < abs(currentInvert))
+			{
+				currentIndex++;
+			}
+			//The point to invert is between currentIndex and currentIndex - 1
+			//Assume linear interpolation between this two points
+			double invertedPos{ (currentIndex - 1) * refTrap.hz + refTrap.hz / 2 + refTrap.hz * (currentInvert - cumulativeAtR[indexR][currentIndex - 1]) / (cumulativeAtR[indexR][currentIndex] - cumulativeAtR[indexR][currentIndex - 1]) };
+			rings.push_back(MacroRing(indexR, invertedPos, distribution(generator)));
+		}
+	}
+	std::cout << "Loading " << rings.size() << " macro-particles from which " << numAtR[0] << " are at r=0.\n";
+	solvePoisson();
+}
+void Plasma::loadDensityFile(std::string fileName, double aTemperature, int numMacro)
+{
+	//The file should just be a single column, in the same way the output of initialDensity is given if you loadProfile for example
+	//Check input makes sense
+	if (aTemperature <= 0)
+	{
+		throw std::logic_error("Temperature needs to be positive");
+	}
+	if (numMacro <= 0)
+	{
+		throw std::logic_error("Number of macro-particles has to be a positive integer");
+	}
+	temperature = aTemperature;
+	//Read the file into the initial density
+	initialDensity.setZero(refTrap.Nz * refTrap.Nr + refTrap.Nr);
+	//Assume the file is in the correct format.
+	double readNumber;
+	std::ifstream rFile(fileName);
+	int currentIndex{ 0 };
+	while (rFile >> readNumber)
+	{
+		initialDensity.coeffRef(currentIndex) = readNumber;
+		++currentIndex;
+	}
+	if (currentIndex != refTrap.Nz * refTrap.Nr + refTrap.Nr)
+	{
+		throw std::logic_error("The number of grid points in the file do not match this trap.");
 	}
 	//Ok, now I have a density grid. I need now to populate macro-particles to match this grid density.
 	//How many particles am I placing at each radial index
