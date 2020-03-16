ac0aeeb9b506cc1c40add33a61fde84849228fb8
Daniel32Duque <59413316+Daniel32Duque@users.noreply.github.com>
2020-03-16 12:29:39 +0000

    Update Constants.hpp

--- Source/Constants.hpp
+++ Source/Constants.hpp
@@ -1,6 +1,6 @@
 /*
 Written by: Daniel Duque
-Last modified on 13 Mar 2020
+Last modified on 16 Mar 2020
 
 Constants needed throughout the different files
 */
@@ -11,6 +11,7 @@ Constants needed throughout the different files
 const double ePos{ 1.602176634e-19 }; //Elementary positive charge in Coulombs
 const double epsilon{ 8.8541878128e-12 }; //Permittivity of free space in SI units
 const double massE{ 9.1093837015e-31 }; //Electron rest mass in kilograms
+const double massP{ 1.67262192369e-27 };//Proton rest mass in kilograms
 const double PI{ 3.141592653589793238463 };
 const double KB{ 1.380649e-23 }; //Boltzmann constant in SI
 
